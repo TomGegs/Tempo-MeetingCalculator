611d7292171db7adcd2d98e47d1c02b9f6e71779
TomGegs <tomgegs@outlook.com>
2024-03-28 13:02:37 +1000

    Fix hover context and remove LoadingAnimation component

--- src/@/components/utils/hoverContext.tsx
+++ src/@/components/utils/hoverContext.tsx
@@ -2,6 +2,7 @@ import { createContext } from 'react';
 
 const HoverContext = createContext({
     isHovered: false,
+    // @ts-expect-error TS6133
     setIsHovered: (isHovered: boolean) => {},
 });
 
--- src/@/ui/LoadingAnimation.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const LoadingAnimation = (props: Props) => {
-    return <div>LoadingAnimation</div>;
-};
-
-export default LoadingAnimation;
